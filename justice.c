370f67c3289bc1a1702c52ac8420e5ca8c5a7b45
seanwu <seanwupi@gmail.com>
2016-01-05 23:16:27 +0800

    Auto-tuning

--- justice.c
+++ justice.c
@@ -1,21 +1,15 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 #include <sys/mman.h>
 
 long memtotal() {
   char buf[1024];
   char name[256];
-  long value, sum;
+  long value, sum = 0;
   FILE *f = fopen("/proc/meminfo", "r");
-  sum = 0;
   while (fgets(buf, sizeof(buf), f)) {
     sscanf(buf, "%s%ld", name, &value);
-    if (!strcmp(name, "MemAvailable:")) {
+    if (!strcmp(name, "MemAvailable:") || !strcmp(name, "SwapFree:"))
       sum += value;
-    } else if(!strcmp(name, "SwapFree:")) {
-      sum += value;
-    }
   }
   fclose(f);
   return sum * 1024;
@@ -23,18 +17,22 @@ long memtotal() {
 
 int main(int argc, char **argv) {
   const int PAGE_SZ = 4096;
-  const int R_SZ = PAGE_SZ * 128000;
-  long i, j, n = memtotal() / R_SZ;
-  printf("N = %d\n", n);
-  for (i=0; argv[0][i]; ++i) argv[0][i] = '\r';
-  for (i=0; i<n; ++i) {
-    if (!fork()) {
-      sleep(1);
-      char *p = (char*)mmap(0, R_SZ, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-      for (j=0; j<R_SZ; j+=PAGE_SZ) p[j] = 1;
-      sleep(60);
-      munmap(p, R_SZ);
-      return 0;
+  const int R_SZ = PAGE_SZ * 64000;
+  long i, j;
+  printf("Process Name: ");
+  gets(argv[0]);
+  while (1) {
+    sleep(1);
+    long m = memtotal();
+    printf("mem = %ldM\n", m / 1024 / 1024);
+    if (m>R_SZ) {
+      if (!fork()) {
+        char *p = (char*)mmap(0, R_SZ, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+        for (j=0; j<R_SZ; j+=PAGE_SZ) p[j] = 1;
+        sleep(900); // 15min
+        munmap(p, R_SZ);
+        return 0;
+      }
     }
   }
 }
